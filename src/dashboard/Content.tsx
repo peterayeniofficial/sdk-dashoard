e7a915691fccee816ccdbda381c1be3206545088
Peter Ayeni <ayenicology@gmail.com>
2022-02-20 16:47:02 +0000

    fix: sdksData undefine during intial load

--- src/dashboard/Content.tsx
+++ src/dashboard/Content.tsx
@@ -14,13 +14,13 @@ interface ContentProps {
 }
 
 export default function Content({ sdksData }: ContentProps) {
-  const categories = sdksData.map((sdk) => sdk.categories[0]);
+  const categories = sdksData?.map((sdk) => sdk.categories[0]);
   const uniqueCategories = Array.from(new Set(categories));
 
   return (
     <Box maxW="7xl" mx={'auto'} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
-        {uniqueCategories.map((cat) => (
+        {uniqueCategories?.map((cat) => (
           <Stat
             px={{ base: 4, md: 8 }}
             py={'5'}
