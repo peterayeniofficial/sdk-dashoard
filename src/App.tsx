4e61dddc36c99f0d00950defdf8a62c7411b565a
Peter Ayeni <ayenicology@gmail.com>
2022-02-20 09:46:27 +0000

    refactor: remove on use code
    Remove on code and add margin to main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Container, Heading } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import Dashboard from './dashboard';
 
 function App() {
   return (
-    <Container maxW={'5xl'}>
-      <Heading>SDK Dashboard</Heading>
+    <Container maxW={'5xl'} mt={8}>
       <Dashboard />
     </Container>
   );
