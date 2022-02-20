5756f5afe93af19909eb7f0ce0374e4b22b7e267
Peter Ayeni <ayenicology@gmail.com>
2022-02-20 10:53:22 +0000

    chore: configure react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Container } from '@chakra-ui/react';
 import Dashboard from './dashboard';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <Container maxW={'5xl'} mt={8}>
-      <Dashboard />
+      <QueryClientProvider client={queryClient}>
+        <Dashboard />
+      </QueryClientProvider>
     </Container>
   );
 }
